83dca74a721bc34c26b6d15fdd71d6f463d1a992
Neveark <ne@kagehira.icu>
2025-06-01 04:15:48 +1000

    Headless config for autobuild

--- Grade_Predictor/src/main/java/org/example/grade_predictor/Launcher.java
+++ Grade_Predictor/src/main/java/org/example/grade_predictor/Launcher.java
@@ -2,6 +2,11 @@ package org.example.grade_predictor;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.setProperty("javafx.verbose", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("java.awt.headless", "true");
+        
         HelloApplication.main(args);
     }
 } 
